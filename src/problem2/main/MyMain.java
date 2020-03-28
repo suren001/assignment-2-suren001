43891694396d7988fda5f1e9e11ef83056b62f52
surendra singh <surendra.singh_cs18@gla.ac.in>
2020-03-28 11:11:33 +0530

    using preVSpost method of binarysearchtree class

--- src/problem2/main/MyMain.java
+++ src/problem2/main/MyMain.java
@@ -7,5 +7,19 @@
 package problem2.main;
 // executable class
 // use problem1.mybst.MyBinarySearchTree class for creating binary tree
+import problem1.mybst.MyBinarySearchTree;
+
 public class MyMain {
+    public static void main(String[] args) {
+        MyBinarySearchTree obj = new MyBinarySearchTree();
+        obj.insert(5);
+        obj.insert(10);
+        obj.insert(1);
+        obj.insert(6);
+        obj.insert(50);
+        obj.insert(40);
+        obj.insert(30);
+        obj.preVSpost();
+    }
+
 }
