3704d2d9bddd83d1b84545fb2a8caa205df0fb4c
surendra singh <surendra.singh_cs18@gla.ac.in>
2020-03-28 11:47:11 +0530

    node class

--- src/problem5/node/Node.java
+++ src/problem5/node/Node.java
@@ -6,5 +6,28 @@
  */
 package problem5.node;
 // to define node properties
+
+import problem5.student.Student;
+
+// to define node properties
 public class Node {
+    Student student;
+    Node next;
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
 }
+
