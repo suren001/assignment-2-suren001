a36b4b834798f6b8ed8a19b4ed69cb227b631b32
surendra singh <surendra.singh_cs18@gla.ac.in>
2020-03-28 09:46:24 +0530

    created data,left and right field getter and setter

--- src/problem1/node/TreeNode.java
+++ src/problem1/node/TreeNode.java
@@ -10,4 +10,27 @@ public class TreeNode {
     private int data;
     private TreeNode left;
     private TreeNode right;
+    public void setdata(int data){
+        this.data=data;
+    }
+
+    public void setLeft(TreeNode left) {
+        this.left = left;
+    }
+
+    public void setRight(TreeNode right) {
+        this.right = right;
+    }
+
+    public int getData() {
+        return data;
+    }
+
+    public TreeNode getLeft() {
+        return left;
+    }
+
+    public TreeNode getRight() {
+        return right;
+    }
 }
