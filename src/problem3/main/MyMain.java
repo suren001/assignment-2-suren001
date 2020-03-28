835135b3523e258f89fa067f2baddcfffd241d29
surendra singh <surendra.singh_cs18@gla.ac.in>
2020-03-28 11:25:17 +0530

    executable class

--- src/problem3/main/MyMain.java
+++ src/problem3/main/MyMain.java
@@ -7,5 +7,16 @@
 package problem3.main;
 // executable class
 // use problem5.student.Student class to create object of student
+import problem3.myqueue.MyPriorityQueue;
+
 public class MyMain {
+    public static void main(String[] args) {
+        MyPriorityQueue obj = new MyPriorityQueue();
+        obj.insert("samar", 1);
+        obj.insert("sameer", 4);
+        obj.insert("sudheer", 3);
+        obj.insert("samrat", 2);
+        obj.insert("surendra", 5);
+        obj.display();
+    }
 }
